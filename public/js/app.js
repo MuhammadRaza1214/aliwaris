e99a3655232ce6b1dc180d022bc1708ccd97f23d
muhammadraza1472 <mohammd.raza@gmail.com>
2016-07-05 13:53:31 +0500

    sdfsdfs

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,23 @@ var socket = io();
 socket.on('connect' , function (){
 	console.log('connected to socket.io server!');
 })
+
+socket.on('message' , function(message){
+	console.log('New message');
+	console.log(messge.text);
+});
+
+var $form = jquery('#message-form');
+
+$form.on('submit', function (event){
+
+	event.prventDefault();
+
+	var $message = $form.find('input[name=message]');
+
+	socket.emit('message', {
+		text: $message.val()
+	});
+
+	$message.val('');
+});
